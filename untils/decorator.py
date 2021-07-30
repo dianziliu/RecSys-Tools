c5565c5ac6acfc04405fb14ce0af45a9292b5652
Liu Junrui <dianziliu@outlook.com>
2021-07-30 16:07:05 +0800

    Create decorator.py

---
+++ untils/decorator.py
@@ -0,0 +1,62 @@
+import time
+import inspect
+from functools import wraps
+from typing import Tuple
+
+step=4
+chap=" "*step
+deep=0
+
+
+def arg_value(arg_name, f, args, kwargs):
+    if arg_name in kwargs:
+        return kwargs[arg_name]
+
+    i = f.__code__.co_varnames.index(arg_name)
+    if i < len(args):
+        return args[i]
+
+    return inspect.signature(f).parameters[arg_name].default
+
+
+def logger(begin_message: str = None, log_args: Tuple[str] = None,
+           end_message: str = None, log_time: bool = True):
+
+    def logger_decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            global deep
+            if begin_message is not None:
+                print(chap*deep+begin_message, end='\n' if log_args is None else '  ')
+                deep+=1
+            if log_args is not None:
+                arg_logs = [arg_name + '=' + str(arg_value(arg_name, f, args, kwargs)) for arg_name in log_args]
+                print(', '.join(arg_logs))
+
+            start_time = time.time()
+            result = f(*args, **kwargs)
+            spent_time = time.time() - start_time
+
+            if end_message is not None:
+                deep-=1
+                print(chap*deep+end_message)
+                
+            if log_time:
+                print('（耗时', spent_time, '秒）', sep='')
+
+            return result
+        return decorated
+    return logger_decorator
+
+@logger("test",("x"),"finishede")
+def test(x):
+    print(x)
+    test2("1")
+
+@logger("test",("x"),"finishede")
+def test2(x):
+    print("helo",x)
+if __name__=='__main__':
+
+    test("hello,world")
+    pass
